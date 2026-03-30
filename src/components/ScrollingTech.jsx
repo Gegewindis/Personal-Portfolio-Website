8d2317bf1405341317cc29f5567aa96c7d06655b
Gegewindis <georgebobocea2005@gmail.com>
2026-03-30 13:08:14 +0200

    fixed the tech scrolling for bigger screens

--- src/components/ScrollingTech.jsx
+++ src/components/ScrollingTech.jsx
@@ -7,25 +7,25 @@ function ScrollingTech() {
         ["devicon-c-original", "C"],
         ["devicon-javascript-plain", "JavaSript"]
     ]
+    const boxLen = 350
+
+    const row = []
+    for (let i = 0; i < Math.ceil(window.innerWidth/boxLen) * 2; i++) {
+        row.push(techs[i % techs.length])
+    }
+
+
 
     return (
         <div style={{ overflow: "hidden" }}>
             <div className="tech-scroll-container">
-                {techs.map((tech, index) =>
+                {row.map((tech, index) =>
                     <div key={index} className="tech-scroll-item">
                         <i className={tech[0]}></i>
                         <h1>{tech[1]}</h1>
 
                     </div>
                 )}
-                {techs.map((tech, index) =>
-                    <div key={index} className="tech-scroll-item">
-                        <i className={tech[0]}></i>
-                        <h1>{tech[1]}</h1>
-
-                    </div>
-                )}
-
             </div>
         </div>
     );
